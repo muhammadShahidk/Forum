397afa0e969c9d81e62f28b469a4f7344460cfd6
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-11-26 22:25:27 +0500

    routing paths configure

---
+++ src/app/routes/paths.ts
@@ -0,0 +1,8 @@
+
+export const _basePath = 'https://localhost:44344/api';
+
+export const PATH_AUTH = {
+  login: 'Auth/login',
+  register: 'Auth/register',
+  allUsersWithRools: 'Auth/get-user-rols',
+};
