7b2f1f60387ced4d799840475b5f2ffe9ff9239f
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-11-26 22:20:07 +0500

    configured httpClient with intercepter

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './intercepters/authIntercepter';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
+  ],
 };
