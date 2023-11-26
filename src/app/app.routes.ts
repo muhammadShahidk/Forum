e482c6f9ccb606e3f640b97f37e39b1ae59f9d51
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-11-26 22:20:30 +0500

    added hom, login , register routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Components/Login/Login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: AppComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
