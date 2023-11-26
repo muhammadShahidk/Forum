4825e1dddaa3fca2f06e7ec9059c69503d036a97
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-11-26 22:19:25 +0500

    signal use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,36 @@
 import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { single } from 'rxjs';
+import { RouterOutlet,RouterLink } from '@angular/router';
 import { LoginComponent } from "./Components/Login/Login.component";
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, LoginComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    LoginComponent,
+    RouterLink,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonToggleModule,
+  ],
 })
 export class AppComponent {
   UpdateNumber() {
-    this.number.update((value)=>value+1);
+    this.number.update((value) => value + 1);
     this.mystate.set(this.number() + 2);
   }
   title = 'Forum';
   mystate = signal(0);
   cal = computed(() => {
-    if(this.number() > 10){
+    if (this.number() > 10) {
       this.mystate() + 2;
-
-    }
-    else{
+    } else {
       this.mystate() + 1;
     }
   });
