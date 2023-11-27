da418e303d7d264eb9e8e2682b5e24243b5b8e82
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-11-27 11:08:43 +0500

    improved code in auth service by adding handle request method

--- src/app/Services/Auth.service.ts
+++ src/app/Services/Auth.service.ts
@@ -10,64 +10,66 @@ import { Observable, firstValueFrom } from 'rxjs';
 })
 export class AuthService {
   Token: string = '';
+
   constructor(private htpp: HttpClient) {}
 
-// create a method to stroe the token in the local storage
-// ...
+  // create a method to store the token in local storage
+  storeToken(token: string): void {
+    console.log('token', token);
+    localStorage.setItem('token', token);
+  }
 
-// create a method to store the token in local storage
-storeToken(token: string): void {
-  console.log('token', token);
-  localStorage.setItem('token', token);
-}
+  // create a method to get the token from local storage
+  getToken(): string {
+    return localStorage.getItem('token') || '';
+  }
 
-// create a method to get the token from local storage
-getToken(): string {
-  return localStorage.getItem('token') || '';
-}
-
-
-  async Register(user: UserRegisterDto): Promise<any> {
+  // create a method to handle API requests
+  private async handleRequest<T>(request: Observable<T>): Promise<T> {
     try {
-      const response = await this.htpp.post(
-        `${_basePath}/${PATH_AUTH.register}`,
-        user
-      );
+      const response = await firstValueFrom(request);
+      console.log(response);
       return response;
     } catch (error) {
-      // Handle the error here
       console.error('An error occurred:', error);
       throw new Error('Something went wrong');
     }
   }
 
-
+  // register method
+  async Register(user: UserRegisterDto): Promise<any> {
+    return this.handleRequest<any>(
+      this.htpp.post(`${_basePath}/${PATH_AUTH.register}`, user)
+    );
+  }
 
   // login method
   async Login(user: UserLoginDto): Promise<any> {
     try {
-      const response = await firstValueFrom(
-        this.htpp.post<loginResponse>(`${_basePath}/${PATH_AUTH.login}`,  user )
+      const response = await this.handleRequest<loginResponse>(
+        this.htpp.post<loginResponse>(`${_basePath}/${PATH_AUTH.login}`, user)
       );
+
       console.log(response.isSucceed);
-      if(response.isSucceed == true){
-        console.log("setting the token")
+      if (response.isSucceed) {
+        console.log("setting the token");
         this.storeToken(response.message);
-      }
-      else{
+      } else {
         console.log(response.message);
         throw new Error(response.message);
       }
+
       return response;
     } catch (error) {
-      // Handle the error here
-      console.log(error)
+      console.log(error);
       const ErrorResult = (error as any).error as loginResponse;
-      console.error('An error occurred:', ErrorResult.message)
+      console.error('An error occurred:', ErrorResult.message);
       throw new Error(`${ErrorResult.message}`);
     }
   }
 }
+
+
 
 interface loginResponse {
   isSucceed:boolean;
