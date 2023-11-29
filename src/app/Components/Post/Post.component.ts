628bd0b239ed2443d8e563fc1fd04f5a5b6b6219
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-11-29 13:03:41 +0500

    Refactor Post component HTML and add create new post button in main component

--- src/app/Components/Post/Post.component.ts
+++ src/app/Components/Post/Post.component.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import {MatChipsModule} from '@angular/material/chips';
+import { PostTailwindcssComponent } from '../PostTailwindcss/PostTailwindcss.component';
 @Component({
   selector: 'app-post',
   standalone: true,
@@ -9,6 +12,9 @@ import {MatCardModule} from '@angular/material/card';
     CommonModule,
     MatButtonModule,
     MatCardModule,
+    MatIconModule,
+    MatChipsModule,
+    PostTailwindcssComponent
   ],
   templateUrl: './Post.component.html',
   styleUrl: './Post.component.css',
@@ -16,4 +22,10 @@ import {MatCardModule} from '@angular/material/card';
 export class PostComponent {
   title:string = "What do you think about manshera"
   user:string ="shahid"
+
+  IconStyles ="text-md text-gray-500"
+
+  Content = `The Shiba Inu is the smallest of the six original and distinct spitz
+  breeds of dog from Japan. A small, agile dog that copes very well with
+  mountainous terrain, the Shiba Inu was originally bred for hunting.`
  }
