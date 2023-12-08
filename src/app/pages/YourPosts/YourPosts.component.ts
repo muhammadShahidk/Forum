348b9b270843c5e3f5ab882a5236eaa8e2b37efe
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-12-08 13:59:37 +0500

    Add YourPosts component files

---
+++ src/app/pages/YourPosts/YourPosts.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-your-posts',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './YourPosts.component.html',
+  styleUrl: './YourPosts.component.css',
+})
+export class YourPostsComponent { }
