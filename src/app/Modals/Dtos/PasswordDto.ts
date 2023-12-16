ca70567b303084365fcc988e4530ed4c2e85ebb2
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2023-12-16 22:11:00 +0500

    Add forgot password interfaces to PasswordDto.ts

--- src/app/Modals/Dtos/PasswordDto.ts
+++ src/app/Modals/Dtos/PasswordDto.ts
@@ -2,3 +2,14 @@ export interface ChangePasswordDto {
   currentPassword: string;
   newPassword: string;
 }
+
+export interface forgotPasswordRequestDto {
+  email: string;
+}
+
+export interface forgotPasswordUpdateDto {
+  userId: string;
+  token: string;
+  newPassword: string;
+  confirmPassword: string;
+}
