81b8b1bdba8768c6cd0e84f8ce222a8f3555b65e
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2024-04-15 18:45:34 +0500

    updated path

--- src/app/routes/paths.ts
+++ src/app/routes/paths.ts
@@ -1,5 +1,5 @@
 // export const _basePath = 'https://localhost:44376/api';
-export const _basePath = 'https://localhost:7001/api';
+export const _basePath = 'https://onlinediscusstionforumdevweb001.azurewebsites.net';
 
 export const Auth = {
   login: 'Auth/login',
